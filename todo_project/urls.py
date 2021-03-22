39d1b425f45388ae416af46dd1a96cd5862e48ac
Tomás Herrou <tomvcp@gmail.com>
2021-03-22 13:59:56 +0100

    first commit

---
+++ todo_project/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('todo/', include('todo_app.urls')),
+    path('accounts/', include('login_app.urls')),
+]
